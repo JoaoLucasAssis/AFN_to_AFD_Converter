4fc4a1077591cb4b3fa3ed69b5fa57d14785f11f
joaolucas <joaolucasdeassis.o@gmail.com>
2024-10-03 21:27:38 -0300

    refactor: Adicionado comentários explicativos para linhas do projeto

--- src/main.py
+++ src/main.py
@@ -14,9 +14,9 @@ def validar_palavras(afd, palavras):
     for palavra in palavras:
         estado_atual = afd['estado_inicial']
         
-        for caractere in palavra:
-            if caractere in afd['transicoes'][estado_atual]:
-                estado_atual = afd['transicoes'][estado_atual][caractere]
+        for caractere in palavra: # ['1', '0', '0', '1']
+            if caractere in afd['transicoes'][estado_atual]: # {'A': {'1': ('A', 'B', 'C'), '0': ('B', 'C')}}
+                estado_atual = afd['transicoes'][estado_atual][caractere] # ('A', 'B', 'C')
             else:
                 resultados.append(False)
                 break
@@ -30,7 +30,7 @@ def validar_palavras(afd, palavras):
 def ler_palavras(caminho_arquivo): 
     with open(caminho_arquivo, 'r') as f:
         linhas = f.readlines()
-    return [list(linha.strip()) for linha in linhas]
+    return [list(linha.strip()) for linha in linhas] # [['0', '1', '0'], ['1', '0', '0', '1']]
 
 def obter_estados_alcancaveis(afnd, estados):
     estados_alcancaveis = set(estados)
@@ -38,22 +38,22 @@ def obter_estados_alcancaveis(afnd, estados):
     
     while fila:
         estado_atual = fila.popleft()
-        if estado_atual in afnd['transicoes'] and 'h' in afnd['transicoes'][estado_atual]:
-            for estado in afnd['transicoes'][estado_atual]['h']:
+        if estado_atual in afnd['transicoes'] and 'h' in afnd['transicoes'][estado_atual]: # {'A': {'1': ['A', 'B'], 'h': ['B', 'C']}}
+            for estado in afnd['transicoes'][estado_atual]['h']: # ['B', 'C']
                 if estado not in estados_alcancaveis:
                     estados_alcancaveis.add(estado)
                 fila.append(estado)
-    return tuple(sorted(estados_alcancaveis))  # Dicionário aceita apenas tuplas como chaves ('A', 'B')
+    return tuple(sorted(estados_alcancaveis))  # Dicionário aceita apenas tuplas como chaves ('B', 'C')
 
 def calcular_transicoes_para_estado(afnd, estado_atual):
     transicoes_por_simbolo = defaultdict(set)
     
-    for estado in estado_atual:
+    for estado in estado_atual: # ('A', 'B')
         if estado in afnd['estados']:
-            for simbolo, proximos_estados in afnd['transicoes'][estado].items():
-                if simbolo != 'h':
-                    for proximo_estado in proximos_estados:
-                        transicoes_por_simbolo[simbolo].update(obter_estados_alcancaveis(afnd, [proximo_estado]))    
+            for simbolo, proximos_estados in afnd['transicoes'][estado].items(): # {'A': {'1': ['A', 'B'], 'h': ['B', 'C']}}
+                if simbolo != 'h': # {'1': ['A', 'B']}
+                    for proximo_estado in proximos_estados: # ['A', 'B']
+                        transicoes_por_simbolo[simbolo].update(obter_estados_alcancaveis(afnd, [proximo_estado])) # ('A', 'B', 'C')
     return transicoes_por_simbolo
 
 def converter_afnd_para_afd(afnd):
@@ -78,10 +78,11 @@ def converter_afnd_para_afd(afnd):
         
         afd['transicoes'][estado_atual] = {}  # {'A': {}}
 
-        transicoes_por_simbolo = calcular_transicoes_para_estado(afnd, estado_atual)  # Cálculo do novo estado através das transições do símbolo {'0': {'A', 'B', 'C'}}
+        # Cálculo do novo estado através das transições do símbolo
+        transicoes_por_simbolo = calcular_transicoes_para_estado(afnd, estado_atual) 
     
         for simbolo, novo_estado in transicoes_por_simbolo.items():
-            novo_estado = tuple(sorted(novo_estado))  # Dicionário aceita apenas tuplas como chaves ('A', 'B')
+            novo_estado = tuple(sorted(novo_estado))  # Dicionário aceita apenas tuplas como chaves ('A', 'B', 'C')
             if novo_estado not in afd['estados']: 
                 afd['estados'].add(novo_estado)  # Adiciona o novo estado descoberto aos estados do afd
                 nao_processados.append(novo_estado)  # Adiciona o novo estado para ser processado
@@ -129,7 +130,7 @@ def ler_afnd(caminho_arquivo):
         estado_atual, simbolo, proximo_estado = linha.strip().split()
         if estado_atual not in afnd['estados'] or proximo_estado not in afnd['estados']:
             raise ValueError(f"Estado inválido: {estado_atual} ou {proximo_estado} não está na lista de estados.")
-        afnd['transicoes'][estado_atual][simbolo].append(proximo_estado)
+        afnd['transicoes'][estado_atual][simbolo].append(proximo_estado) # {'A': {'1': ['A', 'B'], 'h': ['B', 'C']}}
 
     return afnd
 
